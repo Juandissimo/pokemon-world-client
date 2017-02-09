fc949f0d7d7aed12a3cfecc9456a301b6820e9ff
ShadesOfBlack <shadesofblack@mailinator.com>
2017-02-09 17:58:38 +0000

    localhost will not work as a registered server as is it local only

--- config/servers.inc.php
+++ config/servers.inc.php
@@ -6,7 +6,6 @@ $PokemonServers = array (
     'name' => 'Pokemon World',
     'id' => 'simulator',
     'server' => 'sim.psim.online', // sim server
-    // 'server' => 'localhost.psim.online', // pretty sure this can be done. we will cut it out until its needed if it ever is
     'host' => 'sim.psim.online', // guess its another way to run it servers. we havn\'t tried it
     'port' => 8000,
   ),
